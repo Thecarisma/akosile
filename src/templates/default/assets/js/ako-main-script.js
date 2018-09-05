86975002f46783dcc7c462fd5e78fb4e4afbb9c9
Azeez Adewale <azeezadewale98@gmail.com>
2018-09-06 00:02:33 +0100

    re structure source tree :heart:

--- src/templates/default/assets/js/ako-main-script.js
+++
@@ -1,7 +0,0 @@
-function run() {
-	var text = document.getElementById('content').innerHTML.replace(/&gt;/g, ">")  , converter = new showdown.Converter();
-	//converter.setOption('simpleLineBreaks', 'true'); 
-    document.getElementById('content').innerHTML = converter.makeHtml(text);
-	console.log(converter.makeHtml(text));
-}
-run();
